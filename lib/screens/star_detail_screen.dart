21df2bd91d59a7a68cf6ffcda903531d275c7d35
城市星表_之音饲料馆 <1028867764@qq.com>
2025-05-04 10:40:01 +0800

    天玑、description

--- lib/screens/star_detail_screen.dart
+++ lib/screens/star_detail_screen.dart
@@ -24,6 +24,13 @@ class StarDetailScreen extends StatelessWidget {
             // 这里可以添加更多恒星信息，如亮度、距离等
             // 需要扩展Star模型以包含这些信息
             Text('ID: ${star.id}', style: TextStyle(color: Colors.black)),
+            SizedBox(height: 16),
+            // 这里可以添加更多恒星信息，如亮度、距离等
+            // 需要扩展Star模型以包含这些信息
+            Text(
+              '介绍: ${star.description}',
+              style: TextStyle(color: Colors.black),
+            ),
             // 可以添加恒星图片、详细描述等
           ],
         ),
